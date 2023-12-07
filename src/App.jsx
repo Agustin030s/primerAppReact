79e8f5a5bd7b94bb8e631b21d214761e07c70bb8
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-07 10:56:06 -0300

    creamos el componente informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Informacion from "./components/Informacion";
+
 function App() {
-  return <h1>Prueba</h1>;
+  return (
+    <>
+      <h1>Prueba</h1>
+      //Sintaxis para llamar a un componente
+      <Informacion></Informacion>
+      <Informacion/> 
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,11 @@
+
+
+const Informacion = () => {
+    return (
+        <section>
+            <h2>Titulo Secundario</h2>
+        </section>
+    );
+};
+
+export default Informacion;
